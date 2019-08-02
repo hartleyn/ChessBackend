c625bc8e826d349e5c991830007b579b6ebe3971
hartleyn <nhartley18@me.com>
2019-08-02 11:41:27 -0400

    Removed unused middleware import, and converted absolute import to relative.

--- chess_backend/routing.py
+++ chess_backend/routing.py
@@ -1,13 +1,12 @@
-from .middleware import TokenAuthMiddleware, JWTAuthMiddleware
+from .middleware import JWTAuthMiddleware
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chess.routing
+from chess.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-  # Empty for now (http->django views is added by default)
   'websocket': JWTAuthMiddleware(
     URLRouter(
-      chess.routing.websocket_urlpatterns
+      websocket_urlpatterns
     )
   )
 })
