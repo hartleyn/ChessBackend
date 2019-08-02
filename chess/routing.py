db9674e26a9eb2e18d9b32bc41e0d88fe5415897
hartleyn <nhartley18@me.com>
2019-08-02 17:53:02 -0400

    Changed match consumer URL.

--- chess/routing.py
+++ chess/routing.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-  url(r'^chess/matches/lobby/(?P<match_id>[0-9]+)/$', consumers.ChessMatchConsumer),
+  url(r'^ws/chess/matches/lobby/(?P<match_id>[0-9]+)/$', consumers.ChessMatchConsumer),
 ]
